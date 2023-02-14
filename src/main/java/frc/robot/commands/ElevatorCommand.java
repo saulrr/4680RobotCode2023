b861fa487ebff90171b966419353c801ea050af4
team102100 <aztechs10210@gmail.com>
2023-02-13 19:03:40 -0500

    da

--- src/main/java/frc/robot/commands/ElevatorCommand.java
+++ src/main/java/frc/robot/commands/ElevatorCommand.java
@@ -3,10 +3,15 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.XboxController;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.RobotContainer;
+import frc.robot.subsystems.ElevatorSubsystem;
 
 public class ElevatorCommand extends CommandBase {
     private static final double Climb_Speed = 1.0;
+    private ElevatorSubsystem Elevator;
+    private int Num = 1;
 
     public void ClimbCommand(){
         addRequirements(RobotContainer.Elevator);
@@ -20,8 +25,31 @@ public class ElevatorCommand extends CommandBase {
 
     @Override
     public void execute() {
-        if(driveController.getPOV() == 0){
+        if(RobotContainer.shootController.getXButtonPressed()){
+            if(Num == 2){
+                new InstantCommand(() -> Elevator.moveDown(Climb_Speed), Elevator );
+                new InstantCommand(() -> Elevator.stop(), Elevator).withTimeout(1).schedule();
+                Num = 1;
+            }
 
+            
+        }  if(RobotContainer.shootController.getYButtonPressed()){
+            if(Num == 3){
+                new InstantCommand(() -> Elevator.moveDown(Climb_Speed), Elevator );
+                new InstantCommand(() -> Elevator.stop(), Elevator).withTimeout(1).schedule();
+            }
+            new InstantCommand(() -> Elevator.moveUp(Climb_Speed), Elevator );
+            new InstantCommand(() -> Elevator.stop(), Elevator).withTimeout(1).schedule();
+            Num = 2;
+        }
+        if(RobotContainer.shootController.getBButtonPressed()){
+            if(Num == 1){
+                new InstantCommand(() -> Elevator.moveDown(Climb_Speed), Elevator );
+                new InstantCommand(() -> Elevator.stop(), Elevator).withTimeout(2).schedule();
+            }
+            new InstantCommand(() -> Elevator.moveUp(Climb_Speed), Elevator );
+            new InstantCommand(() -> Elevator.stop(), Elevator).withTimeout(1).schedule();
+            Num = 3;
         }
     }
 }
--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,4 +1,8 @@
 package frc.robot.subsystems;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -7,21 +11,25 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ElevatorSubsystem extends SubsystemBase {
    private CANSparkMax motor;
+   private static final int CLIMBER_CAN_ID = 14;
+   TalonFX motor2;
 
    public ElevatorSubsystem() {
+    motor2 = new TalonFX(CLIMBER_CAN_ID);
+    motor2.setNeutralMode(NeutralMode.Brake);
       motor = new CANSparkMax(1, MotorType.kBrushless);
       stop();
    }
 
-   public void moveUp() {
-      motor.set(0.5);
+   public void moveUp(double speed) {
+      motor2.set(ControlMode.PercentOutput  ,speed);
    }
 
-   public void moveDown() {
-      motor.set(-0.5);
+   public void moveDown(double speed) {
+      motor2.set(ControlMode.PercentOutput, -speed);
    }
 
    public void stop() {
-      motor.set(0);
+      motor2.set(ControlMode.PercentOutput, 0);
    }
 }
